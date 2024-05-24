aed1f3d7ac091919f5ca3af74031f00dd254240e
vbalaramesh1 <vbalaramesh@gmail.com>
2024-05-24 19:24:39 +0530

    Add files via upload
    Day 2 Assignment

---
+++ day2/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("BrowserVersion: "+browserVersion);
+		System.out.println("BrowserName: "+browserName);
+		System.out.println("It is: "+isVisible);
+		System.out.println("ReleaseYear : "+releaseYear);
+		System.out.println("BrowserLogo: "+browserLogo);
+	}
+
+}
